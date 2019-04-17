a40f6980a56b6b71d3dda83f5df8c55b8a544d85
hualinghue <liang3633152@gmai.com>
2019-04-17 13:41:29 +0800

    ''

--- core/AGcollect.py
+++ core/AGcollect.py
@@ -155,13 +155,11 @@ class Collect(object):
         file_Iterator = os.walk(path)
         download_file_list = []
         for item in file_Iterator:
-            download_file_list = item[1]
+            download_file_list = item[2]
         self.ftp.cwd("/%s/%s" % (site_name, time))
         file_list = self.ftp.nlst()
         for file_name in file_list:
-            print(file_name,download_file_list)
             if file_name in download_file_list:
-                print(file_name,od)
                 if file_name in od:
                     print(file_name)
                     with open("%s/%s"%(path,file_name), 'r') as f:
