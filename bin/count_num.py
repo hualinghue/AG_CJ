139e7eb4a6780fbaf00778dab98c53b611cd050d
hualinghue <liang3633152@gmai.com>
2019-04-18 20:24:15 +0800

    aaaa

--- bin/count_num.py
+++ bin/count_num.py
@@ -1,10 +1,10 @@
-import os
-
+import os,re
 path = "../files/"
 file_Iterator = os.walk(path)
 re_dic = {}
 for item in file_Iterator:
     for file_name in item[1]:
+        dataType = []
         num = 0
         files_Iterator = os.walk("%s/%s"%(path,file_name))
         for i in files_Iterator:
@@ -15,7 +15,15 @@ for item in file_Iterator:
                         with open("%s/%s/%s/%s"%(path,file_name,time,file),'r') as f:
                             for i in f.readlines():
                                 if i != "run_ok":
+                                    req_dic = {}
+                                    tmp_list = re.findall(' .*?=".*?"', str(i))
+                                    for j in tmp_list:
+                                        key, value = j.split("=")
+                                        req_dic[key.replace(' ', '')] = value.strip('"')
+                                    if req_dic["dataType"] not in dataType:
+                                        dataType.append(req_dic["dataType"])
                                     num += 1
         re_dic[file_name] = num
     break
-print(re_dic)
+print(re_dic,)
+print("dataType:",dataType)
