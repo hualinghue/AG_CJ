fdf76f2c38aeffaec9b685e7444ee13eff18e02b
hualinghue <liang3633152@gmai.com>
2019-04-19 16:40:43 +0800

    asdasd

--- conf/settings.py
+++ conf/settings.py
@@ -19,6 +19,6 @@ DATA_TYPE = {
         "HSR":"tradeNo",   #捕鱼王转账记录
         "HPR":"transferId",  #捕鱼王养鱼游戏记录
         "EBR":"billNO",   #电子游戏下注记录
-        "TR":"transferId",  #户口转账记录
+        "TR":"tradeNo",  #户口转账记录
         "GR":"gmcode",     #游戏结果"GR"仅导出AGIN平台的真人视讯游戏相关数据
 }
