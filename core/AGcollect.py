579e661a2de83fa7dcbc5be5aca176d682e8b1ca
hualinghue <liang3633152@gmai.com>
2019-04-27 00:47:27 +0800

    aa

--- core/AGcollect.py
+++ core/AGcollect.py
@@ -193,7 +193,8 @@ class Collect_handle(object):
             only_ID = date[dataType_obj["type"]]                            #获取数据的唯一键
             table_obj = self.mongo_obj[table_name]
             if table_obj.count() == 0:
-                table_obj.create_index({dataType_obj["type"]: 1})
+                print(table_name,dataType_obj["type"])
+                table_obj.create_index({dataType_obj["type"]:1})
                 print("===================%s=======================" % table_name)
             MDtime = date[dataType_obj["time"]]           #获取时间
             BJtime = datetime.datetime.strptime(MDtime, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=12)
